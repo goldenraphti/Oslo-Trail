6d748a1a9f29f012573d4bff58e1e5d2e3136b1d
Raphaël Ferrand <ferrand@protonmail.ch>
2018-09-07 13:38:16 +0200

    feat: add icons with gradients on home page

--- src/DescriptionHome.js
+++ src/DescriptionHome.js
@@ -8,9 +8,16 @@ const DescriptionHome = () => {
 
               
                    <ul>
-                       <li>10 best trails around Oslo</li>
-                       <li>The most scenic trail running routes hand-picked and tested</li>
-                       <li>Always reachable by public transporttaion</li>
+                       <li>
+                             <img src={require('./images/mountains.png')} />
+                              <p>10 best trails around Oslo</p>
+                      </li>
+                       <li>
+                             <img src={require('./images/eye.png')} />
+                             <p>The most scenic trail running routes hand-picked and tested</p></li>
+                       <li>
+                             <img src={require('./images/bus-stop.png')} />
+                             <p>Always reachable by public transporttaion</p></li>
                    </ul>
 
     )
